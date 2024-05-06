83be515b4ef57b6f57edb6cf623eedf98cb4917f
0x3 <willoxvlad@gmail.com>
2024-05-06 21:03:29 +0300

    bruteforce task

---
+++ bruteforce/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+  "fmt"
+)
+
+var Alphabet = []string{"0", "1"}
+
+func bruteforce(word string, limit int) {
+
+  if limit == 0 {
+    fmt.Println(word)
+  } else {
+    for i, _ := range Alphabet {
+      //fmt.Println(word + Alphabet[i])
+      //time.Sleep(1 * time.Second)
+      bruteforce(word+Alphabet[i], limit-1)
+    }
+  }
+
+}
+
+func main() {
+  word := ""
+  bruteforce(word, 5)
+}
